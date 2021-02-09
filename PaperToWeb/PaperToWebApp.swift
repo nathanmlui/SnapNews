8efeabe3c7da22e223dec95f1cc929ce8e5b0385
nathanmlui <nathanmlui@gmail.com>
2021-02-08 19:18:51 -0800

    Initial Commit

---
+++ PaperToWeb/PaperToWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperToWebApp.swift
+//  PaperToWeb
+//
+//  Created by Nathan Lui on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PaperToWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
